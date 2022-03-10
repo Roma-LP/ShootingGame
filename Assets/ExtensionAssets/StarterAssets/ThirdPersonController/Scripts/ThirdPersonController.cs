ca3d058f3ad6eedbf01668932eee4ec8d4682daf
Roma <rlepesh@mail.ru>
2022-03-10 13:20:58 +0300

    Try add blandtree

--- Assets/ExtensionAssets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/ExtensionAssets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -124,6 +124,9 @@ namespace StarterAssets
             GroundedCheck();
             CrouchLie();
             Move();
+
+            print("_input.move.x" + _input.move.x);
+            print("_input.move.y" + _input.move.y);
         }
 
         private void LateUpdate()
@@ -182,7 +185,7 @@ namespace StarterAssets
             // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is no input, set the target speed to 0
             if (_input.move == Vector2.zero) targetSpeed = 0.0f;
-
+            
             // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
 
